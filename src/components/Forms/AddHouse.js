b8e58392953e53b7140bffdb2ff17af2f599c1c8
Prince Donkor <prince.donkor74@gmail.com>
2020-03-29 11:11:16 -0500

    added file layout

---
+++ src/components/Forms/AddHouse.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddHouse extends Component {
+    render() {
+        return (
+            <div>
+                This is Add a house form page
+            </div>
+        )
+    }
+}
---
+++ src/components/Pages/HouseInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HouseInfo extends Component {
+    render() {
+        return (
+            <div>
+                This is House Info Page
+            </div>
+        )
+    }
+}
