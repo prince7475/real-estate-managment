256aaddcdee5237629e87d639b7fe4f7c4aa9ecc
Prince Donkor <prince.donkor74@gmail.com>
2020-03-29 19:03:31 -0500

    created house card and dashboard page

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,37 +4,20 @@ import DashBoard from './Pages/DashBoard'
 import HomeScreen from './Pages/HomeScreen'
 import TenantInfo from './Pages/TenantInfo'
 import HouseInfo from './Pages/HouseInfo'
+import AddHouse from './Forms/AddHouse'
 
 export default class Main extends Component {
     render() {
         return (
             <div>
                 <Router>
-                    {/* <div>
-                        Temp:
-                        <ul>
-                            <li>
-                                <Link to='/'>HomeScreen</Link>
-                            </li>
-                            <li>
-                                <Link to='/dashboard'>Dashboard</Link>
-                            </li>
-                            <li>
-                                <Link to='/tenantInfo'>TenantInfo</Link>
-                            </li>
-                            <li>
-                                <Link to='/houseInfo'>HouseInfo</Link>
-                            </li>
-                        </ul>
-                    </div> */}
-
-
-
                     <Switch>
                         <Route exact path='/' component={HomeScreen}/>
                         <Route exact path='/dashboard' component={DashBoard}/>
-                        <Route exact path='/tenantInfo' component={TenantInfo}/>
-                        <Route exact path='/houseInfo' component={HouseInfo}/>
+                        <Route exact path='/tenantInfo/:id' component={TenantInfo}/>
+                        <Route exact path='/houseInfo/:id' component={HouseInfo}/>
+                        <Route exact path='/addHouse' component={AddHouse} />
+                        
                     </Switch>
                 </Router>
             </div>
--- src/components/Pages/DashBoard.js
+++ src/components/Pages/DashBoard.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
-
+import HouseCard from '../Cards/HouseCard'
+import {
+    MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol,
+    MDBCardTitle, MDBIcon, MDBModal, MDBModalBody,
+    MDBModalHeader, MDBModalFooter,
+    MDBInput,
+    Link
+} from "mdbreact";
 export default class DashBoard extends Component {
     render() {
         return (
-            <div>
-                This is DashBoard
-            </div>
+            <>
+                <MDBContainer className="text-center">
+                    <div>
+                        <h2 className='text-center pt-3'>Dashboard</h2>
+
+                        <Link to='/addHouse'><MDBBtn>Add A House</MDBBtn></Link>
+                    </div>
+                    <MDBRow>
+                        {[1,2,3,4,5,6,7,8,9].map((el,idx) =>
+                        <MDBCol key={idx}>
+                            <HouseCard />
+                        </MDBCol>
+                            
+                        )}
+                    </MDBRow>
+                </MDBContainer>
+
+            </>
         )
     }
 }
